5d9bd2bc69054b45ae65213754c4792ef4d2dc09
Brunstart <Brunstart@gmail.com>
2022-01-18 00:04:07 +0900

    220118 get api 객체를 미리 정의하고 get 메소르를 받는걸 추천

--- src/main/java/com/example/hello/controller/GetApiController.java
+++ src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -28,5 +31,43 @@ public class GetApiController
     }
 
     // http://localhost:9090/api/get/query-param?user=steve&email=steve@gmail.com&age=30
+    @GetMapping(path="query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam)
+    {
+        StringBuilder sb = new StringBuilder();
 
+        queryParam.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println(("\n"));
+
+            sb.append(entry.getKey()+" = "+entry.getValue()+"\n");
+        });
+
+        return sb.toString();
+    }
+
+    @RequestMapping("query-param02")
+    public String queryParam02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    )
+    {
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name+" "+email+" "+age;
+    }
+
+    @RequestMapping("query-param03")
+    public String queryParam03(UserRequest userRequest)
+    {
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
 }
